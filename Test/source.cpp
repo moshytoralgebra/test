3d8d89206fef3a798fa06547e0a138637cd07435
moshytoralgebra <moshytor@algebra.hr>
2025-04-02 19:14:03 +0200

    first commit

---
+++ Test/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+	cout << "hi";
+	return 0;
+}
